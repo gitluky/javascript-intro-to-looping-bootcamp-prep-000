c820847a8cd9b48696c60d63bc7741c7d3f9c4e7
gitluky <jluk87@gmail.com>
2017-09-09 21:57:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function whileLoop(n){
 
 function maybeTrue() {
   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
-
+}
 
 function doWhileLoop(array){
     do{
