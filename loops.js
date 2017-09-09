c1b17cef55f85b5bf82600a93ea800df6d54efd7
gitluky <jluk87@gmail.com>
2017-09-09 21:58:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,18 +1,18 @@
 function forLoop(array){
   for (var i = 0; i < 25; i++){
     if (i == 1){
-      loopArray.push("I am 1 strange loop")
+      array.push("I am 1 strange loop")
     }
     else {
-      loopArray.push("I am ${i} strange loops")
+      array.push("I am ${i} strange loops")
     }
   }
   return loopArray
 }
 
 function whileLoop(n){
-  while (num >= 0){
-    console.log(--num)
+  while (n >= 0){
+    console.log(--n)
   }
   console.log("done")
 }
